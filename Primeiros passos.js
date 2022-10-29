97552224dd19b9879436dcb030cfb3904cb5f82b
RossanaSena <rossanasena@gmail.com>
2022-10-28 23:30:16 -0300

    Organizar o conhecimento sobre JavaScript

---
+++ Primeiros passos.js
@@ -0,0 +1,16 @@
+/*Principios básicos do JavaScript
+
+- Conhecer e entender os tipos de dados
+
+* Number
+* String
+* Boolean
+
+- Operadores
+
+* Comparação
+* Validação de todas as informações
+* Validação se tem uma OU outra informação
+* Validação de dados inputados junto ao banco de dados
+* Operações matemáticas
+* Operações com stri
