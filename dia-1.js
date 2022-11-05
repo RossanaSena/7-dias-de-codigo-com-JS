b2b0be89185ffcae7a013ccd0f40bedbacbe7e19
RossanaSena <rossanasena@gmail.com>
2022-11-04 22:27:36 -0300

    Dia 1

--- dia-1.js
+++ dia-1.js
@@ -8,21 +8,21 @@ let numeroDez = 10;
 let stringDez = '10';
 
 if (numeroUm == stringUm) {
-    //console.log ('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+    console.log ('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
 } else { 
-    //console.log ('As variáveis numeroUm e stringUm não tem o mesmo valor');
+    console.log ('As variáveis numeroUm e stringUm não tem o mesmo valor');
 }
 
 if (numeroTrinta === stringTrinta) {
-    //console.log ('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
+    console.log ('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
 } else {
-    //console.log ('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+    console.log ('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
 }
   
 if (numeroDez == stringDez) {
-    //console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
 } else {
-    //console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
 }
 
 /*node dia-1.js*/
@@ -35,16 +35,16 @@ const dezAnosDepois = "12"
 const nomeCompleto = nome + sobrenome
 const resultado = idade + dezAnosDepois
 
-//console.log (nomeCompleto)
-//console.log (resultado)
-//console.log (bacharel == 1)
+console.log (nomeCompleto)
+console.log (resultado)
+console.log (bacharel == 1)
 
 const minhaVar = 1;
 const MinhaVar = "texto";
 const minhavar = "3";
 const MINHAVAR = 2;
 
-//console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
 
 const forma = "quadrado";
 const altura = 5;
